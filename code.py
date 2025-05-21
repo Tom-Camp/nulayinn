d222ff93f2bb0aded8238bc7f18a1b936325ed73
Tom Camp <tomcamp@protonmail.ch>
2025-05-21 19:32:25 -0400

    Simplifying.

--- code.py
+++ code.py
@@ -3,8 +3,10 @@ import board
 import busio
 import os
 import time
-from lib.air import Air
-from lib.mox import Mox
+
+from adafruit_ahtx0 import AHTx0
+from adafruit_bme680 import Adafruit_BME680_I2C
+
 from lib.lora_sender import LoraSender
 from lib.battery_monitor import BatteryMonitor
 
@@ -30,26 +32,29 @@ except Exception as e:
 
 
 i2c = busio.I2C(board.A1, board.A0)
-air = Air(i2c_bus=i2c)
-mox = Mox(i2c_bus=i2c)
+air = AHTx0(i2c_bus=i2c)
 
+air_temp = air.temperature
+air_humidity = air.relative_humidity
 
-temperature, humidity = air.read_all()
+mox = Adafruit_BME680_I2C(i2c=i2c, debug=False)
+mox.sea_level_pressure = 1013.25
 
-mox.warm_up(duration=150)
-time.sleep(150/1000)
-coop_temp, coop_humidity, coop_gas, coop_pressure = mox.get_all()
+coop_temp = mox.temperature
+coop_humidity = mox.humidity
+coop_gas = mox.gas
+coop_pressure = mox.pressure
 
 message: dict = {
     "device_id": os.getenv("DEVICE_ID", "unknown"),
     "api_key": os.getenv("API_KEY", "unknown"),
     "data": {
         "outside": {
-            "air_temp": celsius_to_fahrenheit(temperature) - .5,
-            "humidity": humidity,
+            "air_temp": celsius_to_fahrenheit(air_temp) - .5,
+            "humidity": air_humidity,
         },
         "coop": {
-            "coop_temp": celsius_to_fahrenheit(coop_temp) + 9,
+            "coop_temp": celsius_to_fahrenheit(coop_temp),
             "coop_humidity": coop_humidity,
             "coop_gas": coop_gas,
             "coop_pressure": coop_pressure,
